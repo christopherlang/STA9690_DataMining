458ce89ed25df2f07357600ec98c959f6b769723
Christopher Lang <chlang206@gmail.com>
2017-11-09 00:54:50 -0500

    finished question 3

--- midterm/examfiles/takeHome_christopherlang.R
+++ midterm/examfiles/takeHome_christopherlang.R
@@ -1,6 +1,7 @@
 # Install the glmnet package if you haven't done so
 # install.packages("glmnet")
 
+# You many want to comment this out as this is environment specific
 setwd("C:/Users/Christopher Lang/Dropbox/Education/Baruch College/Fall 2017/STA 9690 - Advanced Data Mining/midterm/examfiles")
 
 library(glmnet)
@@ -108,4 +109,51 @@ i2.las <- max(which(cv.las <= cv.las[i1.las] + se.las[i1.las]))
 plot.cv(cv.rid,se.rid,lam.rid,i1.rid,i2.rid)
 plot.cv(cv.las,se.las,lam.las,i1.las,i2.las)
 
+# ||-ANSWER-|| for Part B ======================================================
+
+# For finding ridge regression lambda selection, both rules
+lam.rid[i1.rid]  # usual rule
+lam.rid[i2.rid]  # standard error rule
+
+# For finding lasso regression lambda selection, both rules
+lam.las[i1.las]  # usual rule
+lam.las[i2.las]  # standard error rule
+
+# For finding minimum error for the ridge regression CV
+cv.rid[i1.rid]
+cv.rid[i2.rid]
+
+# For finding minimum error for the lasso regression CV
+cv.las[i1.las]
+cv.las[i2.las]
+
+# ||-ANSWER-|| for Part C ======================================================
+a.rid <- glmnet(X, y, alpha = 0, nlambda = nlam, lambda = lam.rid)
+a.las <- glmnet(X, y, alpha = 1, nlambda = nlam, lambda = lam.las)
+
+rid.beta <- as.matrix(a.rid$beta[,nlam:1])
+las.beta <- as.matrix(a.las$beta[,nlam:1])
+
+plot.image(rid.beta[,i1.rid])
+mtext('Image Ridge Usual Rule')
+
+plot.image(rid.beta[,i2.rid])
+mtext('Image Ridge Standard Error Rule')
+
+plot.image(las.beta[,i1.las])
+mtext('Image Lasso Usual Rule')
+
+plot.image(las.beta[,i2.las])
+mtext('Image Lasso Standard Error Rule')
+
+# ||-ANSWER-|| for Part D ======================================================
+# Ridge regression sum of squared error
+sum((bstar - rid.beta[,i1.rid])^2)  # usual rule, ridge
+sum((bstar - rid.beta[,i2.rid])^2)  # standard error rule, ridge
+
+# Lasso regression sum of squared error
+sum((bstar - las.beta[,i1.las])^2)  # usual rule, lasso
+sum((bstar - las.beta[,i2.las])^2)  # standard error rule, lasso
+
+# You many want to comment this out as this is environment specific
 setwd("C:/Users/Christopher Lang/Dropbox/Education/Baruch College/Fall 2017/STA 9690 - Advanced Data Mining/")
